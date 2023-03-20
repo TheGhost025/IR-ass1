39b4c9b7a7f096d31653de5e0871f99d2f8f3c66
TheGhost025 <mazenkaluoby111@gmail.com>
2023-03-20 15:44:00 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
@@ -10,7 +9,7 @@ public class Main {
 
     private static HashMap<String,DictEntry> index = new HashMap<>();
     public static HashMap<Integer,String> source= new HashMap<>();
-    private static String files[] = {"Frostbite.txt","Godot.txt","Unity.txt","UnrealEngine.txt"};
+    private static String files[] = {"Frostbite.txt","Godot.txt","Unity.txt","UnrealEngine.txt","CyberSecurity.txt","Flutter.txt","GameDevelopment.txt","SoftwareEngneering.txt","Vim.txt","WebDevelopment.txt"};
     static void buildInvertedIndex() throws IOException {
         for(int i = 1; i <= files.length ; i++){
             RandomAccessFile f =new RandomAccessFile(files[i-1],"r");
@@ -22,6 +21,7 @@ public class Main {
                     word = word.toLowerCase();
                     word=word.replace(",","");
                     word=word.replace(".","");
+                    word=word.replace("'s","");
                     if(!index.containsKey(word))
                         index.put(word, new DictEntry());
                     index.get(word).addPosting(i);
@@ -33,21 +33,20 @@ public class Main {
     static void searchWord(String word){
         List<String> result =new ArrayList<String>();
         if(index.containsKey(word)){
-            Posting it =index.get(word).getpList();
-            it.printAll();
+            index.get(word).print();
         }
     }
     public static void main(String[] args) throws IOException {
         buildInvertedIndex();
-        //System.out.println("source\n___________________________________");
-        //for(int key : source.keySet()){
-        //    System.out.println(key + " " + source.get(key));
-        //}
-        //System.out.println("index\n___________________________________");
-        //for(String word : index.keySet()){
-        //    System.out.println("Word: " + word);
-        //    index.get(word).print();
-        //}
+//        System.out.println("source\n___________________________________");
+//        for(int key : source.keySet()){
+//            System.out.println(key + " " + source.get(key));
+//        }
+//        System.out.println("index\n___________________________________");
+//        for(String word : index.keySet()){
+//            System.out.println("Word: " + word);
+//            index.get(word).print();
+//        }
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a word to search: ");
         String word = scanner.nextLine();
